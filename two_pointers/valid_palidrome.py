ea77bd451761de6dcf998def91c0f3d42b69481b
Ching Cheng <ching@chings-air.lan>
2023-04-11 10:33:31 -0700

    valid palimdrone better solution

--- two_pointers/valid_palidrome.py
+++ two_pointers/valid_palidrome.py
@@ -7,16 +7,37 @@
 
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        word = ""
-        for char in s:
-            if char.isalpha():
-                lower_char = char.lower()
-                word += lower_char
-        reverse_word = word[::-1]
-        if word == reverse_word:
-            return True
-        return False
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while not self.alphaNum(s[left]):
+                left += 1
+            while not self.alphaNum(s[right]):
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+        return True
+
+    # own alpha-numeric function
+
+    def alphaNum(self, char):
+        return (
+            ord("A") <= ord(char) <= ord("Z")
+            or ord("a") <= ord(char) <= ord("z")
+            or ord("0") <= ord(char) <= ord("9")
+        )
+
+        # word = ""
+        # for char in s:
+        #     if char.isalpha():
+        #         lower_char = char.lower()
+        #         word += lower_char
+        # reverse_word = word[::-1]
+        # if word == reverse_word:
+        #     return True
+        # return False
