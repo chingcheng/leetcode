54d7724d22123d475a93649b48de41bfa13c4cb0
Ching <mschingcheng@gmail.com>
2023-05-30 16:45:37 -0700

    length of last word

---
+++ arrays_and_hashings/length_of_last_word.py
@@ -0,0 +1,19 @@
+# Given a string s consisting of words and spaces,
+# return the length of the last word in the string.
+
+# A word is a maximal
+# substring
+#  consisting of non-space characters only.
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        i = len(s) -1
+        length = 0
+
+        while s[i] == " ":
+            i -= 1
+        while i >=0 and s[i] != " ":
+            length += 1
+            i -= 1
+        return length
