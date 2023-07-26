a4ead2f32c631d2ba535caeddc29baabb256a9d9
Ching <mschingcheng@gmail.com>
2023-07-26 16:09:52 -0700

    move zeroes

---
+++ two_pointers/move_zeroes.py
@@ -0,0 +1,16 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] == 0:
+                zero = nums.pop(i)
+                nums.append(0)
+            i += 1
